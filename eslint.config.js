fe467c81379d3f4c1eca639a2e3d2285b2b5ba47
yo-ong <45177372+yo-ong@users.noreply.github.com>
2026-07-01 10:23:38 +0900

    chore: 3주차용 린트&프리티어(키보멘토님용) 업데이트

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,53 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import prettier from 'eslint-config-prettier'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       react.configs.flat.recommended,
-      react.configs.flat['jsx-runtime'],
+      react.configs.flat["jsx-runtime"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2022,
+      ecmaVersion: "latest",
       globals: globals.browser,
+      projectService: true,
     },
-    settings: { react: { version: 'detect' } },
+    settings: { react: { version: "detect" } },
     plugins: {
-      'react-hooks': reactHooks,
+      "react-hooks": reactHooks,
     },
     rules: {
-      'react-hooks/exhaustive-deps': 'error',
-      'react-hooks/rules-of-hooks': 'error',
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-non-null-assertion": "error",
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        {
+          assertionStyle: "as",
+          objectLiteralTypeAssertions: "never",
+        },
+      ],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+      "react/prop-types": "off",
+      "react/jsx-no-bind": [
+        "error",
+        {
+          ignoreRefs: true,
+          allowArrowFunctions: true,
+        },
+      ],
     },
   },
   prettier,
-])
+]);
